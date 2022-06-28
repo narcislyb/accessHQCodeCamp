91521ebd4ffd4d1280618f3cf3092f23ba90d682
unknown <693681771@qq.com>
2022-06-28 12:22:59 +1000

    Create postcode test

--- src/test/java/com/accessHQ/api/TestAPISuite.java
+++ src/test/java/com/accessHQ/api/TestAPISuite.java
@@ -3,16 +3,28 @@ package com.accessHQ.api;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
-public class verify_test {
+public class TestAPISuite {
 
     @Test
-    public void verify_status_200() {
+    public void Verify_Status_200() {
         given().
             header("auth-key", "0312d6cd-75cb-4051-9395-0b70ddd076d5").
             param("q", "Tascott").
             param("state", "NSW").
         when().get("https://digitalapi.auspost.com.au/postcode/search.json").
-        then().assertThat().statusCode(201);
+        then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void Verify_Postcode2250() {
+        given().
+            header("auth-key", "0312d6cd-75cb-4051-9395-0b70ddd076d5").
+            param("q", "Tascott").
+            param("state", "NSW").
+        when().get("https://digitalapi.auspost.com.au/postcode/search.json").
+        then().
+            assertThat().body("localities.locality.postcode", equalTo(2250));
     }
 }
